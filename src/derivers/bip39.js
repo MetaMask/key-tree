0f39358ac44553da0eb49717bd78cd7aa29c8cec
Erik Marks <rekmarks@protonmail.com>
2021-02-23 09:48:34 -0800

    Add linting

--- src/derivers/bip39.js
+++ src/derivers/bip39.js
@@ -1,26 +1,26 @@
 // node
-const crypto = require('crypto')
+const crypto = require('crypto');
 // npm
-const bip39 = require('bip39')
+const bip39 = require('bip39');
 
-const ROOT_BASE_SECRET = Buffer.from('Bitcoin seed', 'utf8')
+const ROOT_BASE_SECRET = Buffer.from('Bitcoin seed', 'utf8');
 
 module.exports = {
   // standard
   deriveChildKey,
   // utility
   bip39MnemonicToMultipath,
-}
+};
 
 function bip39MnemonicToMultipath(mnemonic) {
-  return `bip39:${mnemonic.trim()}`
+  return `bip39:${mnemonic.trim()}`;
 }
 
 // this creates a child key using bip39, ignoring the parent key
-function deriveChildKey (_parentKey, pathPart) {
-  const mnemonic = pathPart
-  const seedBuffer = bip39.mnemonicToSeed(mnemonic)
-  const entropy = crypto.createHmac('sha512', ROOT_BASE_SECRET).update(seedBuffer).digest()
+function deriveChildKey(_parentKey, pathPart) {
+  const mnemonic = pathPart;
+  const seedBuffer = bip39.mnemonicToSeed(mnemonic);
+  const entropy = crypto.createHmac('sha512', ROOT_BASE_SECRET).update(seedBuffer).digest();
 
-  return entropy
+  return entropy;
 }
