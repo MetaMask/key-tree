96f1f06f95eade1e33fff07b3e0d4dde1f131a04
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-03-25 23:35:48 +0100

    Fix ESLint issue

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,15 @@
-import * as mod from '.';
+import {
+  MAX_BIP_44_DEPTH,
+  MIN_BIP_44_DEPTH,
+  PackageBuffer,
+  SLIP10Node,
+  secp256k1,
+  ed25519,
+} from '.';
 
 // This is purely for coverage shenanigans
 describe('index', () => {
   it('has expected exports', () => {
-    const { MAX_BIP_44_DEPTH, MIN_BIP_44_DEPTH, PackageBuffer, SLIP10Node, secp256k1, ed25519 } =
-      mod;
-
     expect(MAX_BIP_44_DEPTH).toStrictEqual(5);
     expect(MIN_BIP_44_DEPTH).toStrictEqual(0);
     expect(PackageBuffer).toStrictEqual(Buffer);
