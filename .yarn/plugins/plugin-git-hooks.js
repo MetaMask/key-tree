98ee16f3b2f6a885282664706039f732b4206955
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-09-16 13:17:57 +0200

    Revert "Update plugin"
    This reverts commit 7a0542ceaf77391db40ca492c44b6b2c6609623b.

--- .yarn/plugins/plugin-git-hooks.js
+++ .yarn/plugins/plugin-git-hooks.js
@@ -1,14 +1,7 @@
-const { execute } = require('@yarnpkg/shell');
+const { execSync } = require('child_process');
 
-const afterAllInstalled = async () => {
-  const exitCode = await execute('yarn simple-git-hooks');
-
-  if (exitCode !== 0) {
-    // We have to use `process.exit` here rather than setting `process.exitCode`
-    // because Yarn will override any exit code set in this hook.
-    // eslint-disable-next-line node/no-process-exit
-    process.exit(exitCode);
-  }
+const afterAllInstalled = (project) => {
+  execSync('yarn simple-git-hooks', { cwd: project.cwd, stdio: 'inherit' });
 };
 
 module.exports = {
