bd4ce9b7c39cd5133efe86d2283c96b5f4142fe5
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-02-28 17:51:33 +0100

    Check for private key validity in BIP-39 deriver

--- src/derivers/bip39.ts
+++ src/derivers/bip39.ts
@@ -1,12 +1,12 @@
 import { mnemonicToSeed } from '@metamask/scure-bip39';
 import { wordlist as englishWordlist } from '@metamask/scure-bip39/dist/wordlists/english';
-import { assert, bytesToBigInt } from '@metamask/utils';
+import { assert } from '@metamask/utils';
 import { hmac } from '@noble/hashes/hmac';
 import { sha512 } from '@noble/hashes/sha512';
 
 import { DeriveChildKeyArgs } from '.';
 import { BIP39StringNode, BYTES_KEY_LENGTH } from '../constants';
-import { Curve, secp256k1 } from '../curves';
+import { Curve } from '../curves';
 import { SLIP10Node } from '../SLIP10Node';
 import { getFingerprint } from '../utils';
 
@@ -48,16 +48,20 @@ export async function deriveChildKey({
  */
 export async function createBip39KeyFromSeed(
   seed: Uint8Array,
-  curve: Curve = secp256k1,
+  curve: Curve,
 ): Promise<SLIP10Node> {
+  assert(
+    seed.length >= 16 && seed.length <= 64,
+    'Invalid seed: The seed must be between 16 and 64 bytes long.',
+  );
+
   const key = hmac(sha512, curve.secret, seed);
   const privateKey = key.slice(0, BYTES_KEY_LENGTH);
   const chainCode = key.slice(BYTES_KEY_LENGTH);
 
-  const privateKeyBigInt = bytesToBigInt(privateKey);
   assert(
-    privateKeyBigInt > BigInt(0) && privateKeyBigInt < curve.curve.n,
-    'Invalid private key: The private key must larger than 0 and less than the curve order.',
+    curve.isValidPrivateKey(privateKey),
+    'Invalid private key: The private key must greater than 0 and less than the curve order.',
   );
 
   const masterFingerprint = getFingerprint(
