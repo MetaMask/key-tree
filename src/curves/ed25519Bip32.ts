23bca329e468b7682bec838f6eace6c3cc33f400
PeterBenc <peter.benc1@gmail.com>
2024-03-06 20:43:40 +0100

    fixup! add JSDOCs to ed25519Bip32 file

--- src/curves/ed25519Bip32.ts
+++ src/curves/ed25519Bip32.ts
@@ -35,12 +35,24 @@ export const deriveUnhardenedKeys = true;
 
 export const publicKeyLength = 32;
 
+/**
+ * Converts a Uint8Array of bytes to a bigint in little-endian format.
+ *
+ * @param bytes - The Uint8Array of bytes to convert.
+ * @returns The converted bigint value.
+ */
 const bytesToNumberLE = (bytes: Uint8Array): bigint => {
   return hexToBigInt(bytesToHex(Uint8Array.from(bytes).reverse()));
 };
 
-// equivalent to https://github.com/jedisct1/libsodium/blob/93a6e79750a31bc0b946bf483b2ba1c77f9e94ce/src/libsodium/crypto_scalarmult/ed25519/ref10/scalarmult_ed25519_ref10.c#L105
-// which is used by cardano-js-sdk/crypto https://github.com/input-output-hk/cardano-js-sdk/blob/8a6db2a251cd1c956f52730a0d35de2b7fc67404/packages/crypto/src/Bip32/Bip32PrivateKey.ts#L161
+/**
+ * Multiplies the given key with the base point on the Edwards curve.
+ * equivalent to https://github.com/jedisct1/libsodium/blob/93a6e79750a31bc0b946bf483b2ba1c77f9e94ce/src/libsodium/crypto_scalarmult/ed25519/ref10/scalarmult_ed25519_ref10.c#L105 .
+ * which is used by cardano-js-sdk/crypto https://github.com/input-output-hk/cardano-js-sdk/blob/8a6db2a251cd1c956f52730a0d35de2b7fc67404/packages/crypto/src/Bip32/Bip32PrivateKey.ts#L161 .
+ *
+ * @param key - The key to multiply with the base point.
+ * @returns The resulting point on the Edwards curve.
+ */
 const multiplyWithBase = (key: Uint8Array): Uint8Array => {
   // Little-endian SHA512 with modulo n
   const scalar = mod(bytesToNumberLE(key), curve.n); // The actual scalar
@@ -48,6 +60,13 @@ const multiplyWithBase = (key: Uint8Array): Uint8Array => {
   return point.toRawBytes(); // Uint8Array representation
 };
 
+/**
+ * Calculates the public key corresponding to a given private key.
+ *
+ * @param privateKey - The private key.
+ * @param _compressed - Optional parameter to indicate if the public key should be compressed.
+ * @returns The public key.
+ */
 export const getPublicKey = async (
   privateKey: Uint8Array,
   _compressed?: boolean,
@@ -55,6 +74,13 @@ export const getPublicKey = async (
   return multiplyWithBase(privateKey.slice(0, 32));
 };
 
+/**
+ * Adds a tweak to a public key.
+ *
+ * @param _publicKey - The public key.
+ * @param _tweak - The tweak to add.
+ * @returns The resulting public key.
+ */
 export const publicAdd = (
   _publicKey: Uint8Array,
   _tweak: Uint8Array,
@@ -64,11 +90,23 @@ export const publicAdd = (
     .toRawBytes();
 };
 
+/**
+ * Compresses an Ed25519 public key.
+ *
+ * @param publicKey - The public key to compress.
+ * @returns The compressed public key.
+ */
 export const compressPublicKey = (publicKey: Uint8Array): Uint8Array => {
   // Ed25519 public keys don't have a compressed form.
   return publicKey;
 };
 
+/**
+ * Decompresses a compressed Ed25519Bip32 public key.
+ *
+ * @param publicKey - The compressed public key.
+ * @returns The decompressed public key.
+ */
 export const decompressPublicKey = (publicKey: Uint8Array): Uint8Array => {
   // Ed25519 public keys don't have a compressed form.
   return publicKey;
